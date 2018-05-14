7b48e9f11dc698c2dc9e574c4383eabd78e61f81
Eugene Romankov <iromankov2010@gmail.com>
2018-05-14 18:04:31 +0300

    deleting files

--- gulp/tasks/pug.js
+++ gulp/tasks/pug.js
@@ -8,7 +8,7 @@ module.exports = function() {
             .pipe($.gp.pug({
                 pretty: true,
                 locals: {
-                    plaintext: JSON.parse(fs.readFileSync('./content/content.json', 'utf8'))
+                    plaintext: JSON.parse(fs.readFileSync('./source/template/content/content.json', 'utf8'))
                 }
             }))
             .on('error', $.gp.notify.onError(function(error) {
